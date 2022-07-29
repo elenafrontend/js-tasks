6b3cddb16a1b755b9eb0a54aaef5c9e0194fa254
gumerova.e <gumerova.e@etton.ru>
2022-07-29 17:25:42 +0300

    feat: input file settings

--- upload/upload.js
+++ upload/upload.js
@@ -1,1 +1,30 @@
-console.log('upload.js')
+export default function upload(selector, options = {}) {
+  const input = document.querySelector(selector);
+
+  const openBtn = document.createElement('button');
+  openBtn.classList.add('btn');
+  openBtn.textContent = 'Открыть';
+  input.insertAdjacentElement('afterend', openBtn);
+
+  if (options.multi) {
+    input.setAttribute('multiple', true);
+  }
+
+  if (options.accept && Array.isArray(options.accept)) {
+    input.setAttribute('accept', options.accept.join(','));
+  }
+
+  const triggerInput = () => input.click();
+
+  const changeHandler = (event) => {
+    const files = Array.from(event.target.files);
+    if (!files.length) return;
+
+    files.forEach((file) => {
+      if (!file.type.match('image')) return;
+    });
+  };
+
+  openBtn.addEventListener('click', triggerInput);
+  input.addEventListener('change', changeHandler);
+}
