47d4f083b780a48fb8812903514272a2f141d59a
e.gumerova <elenafrontend@ya.ru>
2024-01-13 20:28:56 +0300

    refactor(indexeddb): extract request db function

--- src/dom/client-side-storage/indexeddb/index.js
+++ src/dom/client-side-storage/indexeddb/index.js
@@ -2,26 +2,24 @@
 // Create an instance of a db object for us to store the open database in
 let db;
 
-window.onload = () => {
-  // Open our database; it is created if it doesn't already exist
-  // (see the upgradeneeded handler below)
+const requestDB = () => {
   const request = window.indexedDB.open("notes", 1);
 
   // error handler signifies that the database didn't open successfully
-  request.onerror = function () {
+  request.addEventListener('error', () => {
     console.log("Database failed to open");
-  };
+  });
 
   // success handler signifies that the database open successfully
-  request.onsuccess = function () {
+  request.addEventListener('success', () => {
     console.log("Database opened successfully");
 
     // Store the opened database object in the db variable
     db = request.result;
-  };
+  });
 
   // Set up the database tables if this has not already been done
-  request.onupgradeneeded = function (e) {
+  request.addEventListener('upgradeneeded', (e) => {
     // Grab a reference to the opened database
     db = e.target.result;
 
@@ -33,10 +31,15 @@ window.onload = () => {
     });
 
     // Define what data items the objectStore will contain
-    objectStore.createIndex("title", "title", { unique: false });
-    objectStore.createIndex("body", "body", { unique: false });
+    objectStore.createIndex("title", "title", {unique: false});
+    objectStore.createIndex("body", "body", {unique: false});
 
     console.log("Database setup complete");
-  };
+  })
 }
+
+window.addEventListener('load', () => {
+  // Open our database; it is created if it doesn't already exist
+  requestDB();
+})
 
