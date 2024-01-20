2cf79d30c4b954011bee9e0d650bd0c1bc92e765
e.gumerova <elenafrontend@ya.ru>
2024-01-20 13:19:43 +0300

    refactor(numbers): update files structure
    + fix lint errors

--- src/numbers/numFormat.js
+++ src/numbers/numFormat.js
@@ -3,10 +3,10 @@ export const addThousandsSeparator = (value) => {
   const regexp = /(\d)(?=(\d\d\d)+([^\d]|$))/g;
 
   if (str.includes(".")) {
-    /*const integer = str.substr(0, str.indexOf("."));
-    const decimal = str.substr(str.indexOf(".") + 1);
-    const formatInteger = integer.replace(regexp, "$1\u00A0"); //narrow no-break space \u202F
-    return `${formatInteger},${decimal}`;*/
+    // const integer = str.substr(0, str.indexOf("."));
+    // const decimal = str.substr(str.indexOf(".") + 1);
+    // const formatInteger = integer.replace(regexp, "$1\u00A0"); //narrow no-break space \u202F
+    // return `${formatInteger},${decimal}`;
 
     const numberParts = str.split(".");
     const integer = numberParts[0];
