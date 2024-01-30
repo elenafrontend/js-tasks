283467072e2119c4fcc93a56b54ec98241b8ebf0
e.gumerova <elenafrontend@ya.ru>
2024-01-30 17:18:03 +0300

    refactor(types/set): create extended set class for basic set operations

--- src/types/index.js
+++ src/types/index.js
@@ -1,6 +1,6 @@
 import { Split } from "./symbol/index.js";
 import { removeAnagramDuplicates } from "./map/index.js";
-import { uniqueArray } from "./set/index.js";
+import { uniqueArray, ExtendedSet } from "./set/index.js";
 
 alert('Use console for demo examples');
 
@@ -21,6 +21,23 @@ const values = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 console.log(`Removing duplicates from [${values}]`, uniqueArray(values))
+
+console.group('Implementing basic set operations');
+const extSetA = new ExtendedSet([1, 2, 3, 4]);
+const extSetBigA = new ExtendedSet([...Array(1000)].map((_, i) => i + 1));
+const setB = new Set([6, 7, 9, 10]);
+const setBigB = new Set([...Array(10100)].map((_, i) => i + 1 + 100));
+const setC = new Set([3, 4, 5, 6]);
+
+console.log(`intersection {${[...extSetA]}} and {${[...setC]}} => `);
+console.log(extSetA.intersection(setC))
+console.log(`intersection SetA 1000 & SetB 10000 elem => `);
+console.log(extSetBigA.intersection(setBigB))
+
+console.log(`ExtendedSet call chain symmetricDifference between {${[...extSetA]}} and {${[...setC]}} + union {${[...setB]}}`);
+console.log(extSetA.symmetricDifference(setC).union(setB))
+console.groupEnd('Implementing basic set operations');
+
 console.groupEnd('Set');
 
 
--- src/types/set/index.js
+++ src/types/set/index.js
@@ -16,77 +16,66 @@ export function uniqueArray(arr) {
 /* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
 // Disable the ForOfStatement rule for implementing the for of loop with a Set object.
 
-// --------------------- union
-export function union(setA, setB) {
-  return new Set([...setA, ...setB]);
-}
-
-// --------------------- difference
-export function difference(setA, setB) {
-  const result = new Set(setA);
-  
-  // eslint-disable-next-line no-restricted-syntax
-  for(const item of setB) {
-    result.delete(item);
+export class ExtendedSet extends Set {
+  // --------------------- union
+  union(setB) {
+    return new ExtendedSet([...this, ...setB]);
   }
-  return result;
-}
 
-// --------------------- symmetricDifference
-export function symmetricDifference(setA, setB) {
-  const result = new Set(setA);
+  // --------------------- difference
+  difference(setB) {
+    const result = new ExtendedSet(this);
 
-  console.group('SymmetricDifference');
-  console.time('For of');
-  for( const item of setB) {
-    if (!result.delete(item)) {
-      result.add(item);
+    for(const item of setB) {
+      result.delete(item);
     }
+    return result;
   }
-  console.timeEnd('For of');
 
-  console.time('forEach');
-  setB.forEach(item => {
-    if (!result.delete(item)) {
-      result.add(item);
+  // --------------------- symmetricDifference
+  symmetricDifference(setB) {
+    let result = new ExtendedSet(this);
+
+    console.group('SymmetricDifference');
+    console.time('For of');
+    for( const item of setB) {
+      if (!result.delete(item)) {
+        result.add(item);
+      }
     }
-  })
-  console.timeEnd('forEach');
+    console.timeEnd('For of');
 
-  console.groupEnd('SymmetricDifference');
-  return result;
-}
+    result = new ExtendedSet(this);
+    console.time('forEach');
+    setB.forEach(item => {
+      if (!result.delete(item)) {
+        result.add(item);
+      }
+    })
+    console.timeEnd('forEach');
 
-
-// --------------------- intersection
-export function intersection(setA, setB) {
-  console.group('Set intersection');
-  console.time('For of');
-  let result = new Set();
-
-  for (const item of setB) {
-    if (setA.has(item)) {
-      result.add(item);
-    }
+    console.groupEnd('SymmetricDifference');
+    return result;
   }
-  console.timeEnd('For of');
 
-  console.time('Array');
-  result = new Set([...setA].filter(elem => setB.has(elem)));
-  console.timeEnd('Array');
+  // --------------------- intersection
+  intersection(setB) {
+    console.group('Set intersection');
+    console.time('For of');
+    let result = new ExtendedSet();
 
-  console.groupEnd('Set intersection');
-  return result;
+    for (const item of setB) {
+      if (this.has(item)) {
+        result.add(item);
+      }
+    }
+    console.timeEnd('For of');
+
+    console.time('Array');
+    result = new ExtendedSet([...this].filter(elem => setB.has(elem)));
+    console.timeEnd('Array');
+
+    console.groupEnd('Set intersection');
+    return result;
+  }
 }
-
-
-const setA = new Set([1, 2, 3, 4]);
-const setC = new Set([3, 4, 5, 6]);
-const setBigA = new Set([...Array(1000)].map((_, i) => i + 1));
-const setBigB = new Set([...Array(10100)].map((_, i) => i + 1 + 100));
-
-console.log(`union {${[...setA]}} and {${[...setC]}}`, union(setA, setC));
-console.log('difference', difference(setA, setC));
-console.log(`symmetricDifference between {${[...setA]}} and {${[...setC]}} => `, symmetricDifference(setBigA, setBigB)); // returns Set {1, 2, 5, 6});
-console.log(`intersection {${[...setA]}} and {${[...setC]}} => `, intersection(setA, setC));
-console.log(`intersection SetA 1000 & SetB 10000 elem => `, intersection(setBigA, setBigB));
